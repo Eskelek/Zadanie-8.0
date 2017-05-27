a7cc903d63e18829fd446251fe73f6a358fc543d
Eskelek <lorthan966@wp.pl>
2017-05-27 09:53:22 +0200

    triangleArea

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+var a = 5,
+    h = 7,
+    triangleArean = a*h/2;
+
+console.log('triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArean);
+
+var show = prompt("triangle field: " + triangleArean + " ...I'm right?");
