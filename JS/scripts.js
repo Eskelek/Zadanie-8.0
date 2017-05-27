cb2902d6381d656034d87618633a673656fde3ab
Eskelek <lorthan966@wp.pl>
2017-05-27 14:26:08 +0200

    8.6

---
+++ JS/scripts.js
@@ -0,0 +1,15 @@
+var a = 5,
+    b = 10,
+    value = (a * a) + (2 * a * b) - (b * b);
+
+console.log("Value: " + value);
+
+if (value > 0) {
+    console.log("wynik dodatni");
+}
+else if (value < 0) {
+    console.log("wynik ujemny");
+}
+else {
+    console.log("wynik równy 0");
+}
